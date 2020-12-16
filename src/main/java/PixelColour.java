241ea86457059b5d27aa10151f4e68eae7c805ac
normaloide <edoardoalejandroferrari@gmail.com>
2020-12-16 12:07:20 +0100

    RED: created repo

---
+++ src/main/java/PixelColour.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+
+public class PixelColour {
+
+    public PixelColour() {
+
+    }
+
+    public Color getColour() {
+        return new Color(0, 0, 0);
+    }
+}
---
+++ src/test/java/PixelColourTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class PixelColourTest {
+
+    @Test
+    public void getColourTest() {
+        fail("Not yet implemented");
+    }
+}
