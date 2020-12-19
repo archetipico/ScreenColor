e89d3d41019824b214dd7cfea793b91d1c5eceec
normaloide <edoardoalejandroferrari@gmail.com>
2020-12-19 14:08:02 +0100

    Moved folder

--- src/test/java/ScreenColorTests.java
+++ src/test/java/ScreenColorTests.java
@@ -13,11 +13,11 @@ public class ScreenColorTests {
     @Test
     public void testGetColor() {
         try {
-            BufferedImage pic0 = ImageIO.read(new File("src/images/antelope-canyon-640x480.jpg"));
-            BufferedImage pic1 = ImageIO.read(new File("src/images/french-desert-1599x1066.jpg"));
-            BufferedImage pic2 = ImageIO.read(new File("src/images/lotus-flower-1279x852.jpg"));
-            BufferedImage pic3 = ImageIO.read(new File("src/images/oranges-2500x1667.jpg"));
-            BufferedImage pic4 = ImageIO.read(new File("src/images/scsi-hd-1920x1440.jpg"));
+            BufferedImage pic0 = ImageIO.read(new File("src/test/images/antelope-canyon-640x480.jpg"));
+            BufferedImage pic1 = ImageIO.read(new File("src/test/images/french-desert-1599x1066.jpg"));
+            BufferedImage pic2 = ImageIO.read(new File("src/test/images/lotus-flower-1279x852.jpg"));
+            BufferedImage pic3 = ImageIO.read(new File("src/test/images/oranges-2500x1667.jpg"));
+            BufferedImage pic4 = ImageIO.read(new File("src/test/images/scsi-hd-1920x1440.jpg"));
 
             Color color0 = new ScreenColor(pic0).getColor();
             Color color1 = new ScreenColor(pic1).getColor();
@@ -38,7 +38,7 @@ public class ScreenColorTests {
     @Test
     public void testGetGradient() {
         try {
-            BufferedImage pic = ImageIO.read(new File("src/images/lotus-flower-1279x852.jpg"));
+            BufferedImage pic = ImageIO.read(new File("src/test/images/lotus-flower-1279x852.jpg"));
 
             Color testColor = new Color(255, 255, 255);
             ArrayList<Color> gradient = new ScreenColor(pic).getGradient(testColor);
